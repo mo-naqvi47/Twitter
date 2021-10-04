475708c9fb486d304ad259076d73e8cb6855cf41
mo-naqvi47 <monaqvi1992@gmail.com>
2021-10-04 17:15:54 -0400

    update 10/4 - 3

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -36,13 +36,28 @@ class TweetCellTableViewCell: UITableViewCell {
     }
     
     @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error is retweeeting: \(error)")
+        })
     }
-
     
     
     var favorited: Bool = false
-    
     var tweetId:Int = -1
+    
+    
+    func setRetweeted(_ isRetweeted:Bool){
+        if(isRetweeted) {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else{
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+        
+    }
     
     
     
